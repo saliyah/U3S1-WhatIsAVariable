0db616160c85027f857c5a1aef71ca691e1b60b5
saliyah <52254232+saliyah@users.noreply.github.com>
2025-03-06 14:54:21 -0500

    3/6 2:54

--- src/main/java/com/codedifferently/lab/partA/JavaLibs.java
+++ src/main/java/com/codedifferently/lab/partA/JavaLibs.java
@@ -1,16 +1,16 @@
-package com.codedifferently.lab.partA;
+package com.codedifferently.lab.partA; //package where class is located
 
-public class JavaLibs {
-    public static void main(String[] args) {
-        String x = "Sams";
-        System.out.println("My name is " +x);
-        System.out.println(x +" is so cool!");
-        System.out.println("Hooray " +x +"!");
-        System.out.println("Please enjoy " +x +" Appreciation "
-                + "Day! My name is " +x +". I know how excited "
-                + "everyone is to start appreciating " +x +" on " +x
-                + " Appreciation Day! " +x +", " +x +", " +x +"! Yay "
-                + x +"!!! That's me! " +x +" is the best date ever!");
+public class JavaLibs { //public class JavaLibs
+    public static void main(String[] args) { // program starts
+        String x = "Sams"; // string variable 'x' and assigns to value "Sams"
+        System.out.println("My name is " +x); // string and vaule are concatenate to create "my name is sams"
+        System.out.println(x +" is so cool!"); //
+        System.out.println("Hooray " +x +"!"); //
+        System.out.println("Please enjoy " +x +" Appreciation " //
+                + "Day! My name is " +x +". I know how excited " //
+                + "everyone is to start appreciating " +x +" on " +x //
+                + " Appreciation Day! " +x +", " +x +", " +x +"! Yay " //
+                + x +"!!! That's me! " +x +" is the best date ever!"); //
 
     }
 }
